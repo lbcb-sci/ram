63aa25e5e2708537a4058ef8e647dfe338368176
rvaser <robert.vaser@fer.hr>
2019-09-02 22:35:29 +0200

    Bump version number

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 
 #include "ram/ram.hpp"
 
-static const std::string version = "v0.0.9";
+static const std::string version = "v0.0.10";
 
 static struct option options[] = {
     {"kmer-length", required_argument, nullptr, 'k'},
